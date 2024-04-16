fcca54c30e858ff4d110bb8f8f39b80db375a699
Thiago Barcelos <thiagolbf@hotmail.com>
2024-04-15 22:34:58 -0300

    feat: add variant for buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { Button } from "./components/common/Button";
+import { ButtonVariant } from "./components/common/Button/Button.constants";
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <button onClick={() => setCount((count) => count + 1)}>
-      count is {count}
-    </button>
-  )
+    <>
+      {count}
+
+      <Button onClick={() => setCount(count + 1)}>Teste</Button>
+      <Button variant={ButtonVariant.Default}>Default</Button>
+      <Button variant={ButtonVariant.Outlined}>Outlined</Button>
+      <Button variant={ButtonVariant.Text}>Text</Button>
+      <Button variant={ButtonVariant.Disabled}>Disabled</Button>
+    </>
+  );
 }
---
+++ src/components/common/Button/Button.tsx
@@ -0,0 +1,36 @@
+import { ButtonHTMLAttributes } from "react";
+import styles from "./Button.module.css";
+import { ButtonVariant } from "./Button.constants";
+
+interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: ButtonVariant;
+}
+
+export function Button({
+  variant = ButtonVariant.Default,
+  children,
+  ...rest
+}: IButton) {
+  let buttonClass = styles.buttonBase;
+
+  switch (variant) {
+    case ButtonVariant.Default:
+      buttonClass += ` ${styles.buttonDefault}`; //cuidado com espaço
+      break;
+    case ButtonVariant.Disabled:
+      buttonClass += ` ${styles.buttonDisabled}`;
+      break;
+    case ButtonVariant.Outlined:
+      buttonClass += ` ${styles.buttonOutlined}`;
+      break;
+    case ButtonVariant.Text:
+      buttonClass += ` ${styles.buttonText}`;
+      break;
+  }
+
+  return (
+    <button className={buttonClass} {...rest}>
+      {children}
+    </button>
+  );
+}
